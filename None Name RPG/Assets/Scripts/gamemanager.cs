fb1fb67d6b5efcec23114a96829c25408a5203ab
Zoryx <15801610913@163.com>
2018-12-17 12:27:52 +0800

    no message

--- None Name RPG/Assets/Scripts/gamemanager.cs
+++ None Name RPG/Assets/Scripts/gamemanager.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class gamemanager : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+    public Way way1;
+    public Way way2;
+    public Way way3;
+    public Way way4;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+
 	}
 }
